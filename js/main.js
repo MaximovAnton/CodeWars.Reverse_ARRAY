fa22094530e91a9ff5da00356049845e03bdc712
anton <maximov.antonn@gmail.com>
2022-04-10 20:59:07 +0300

    Second array

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.querySelector('.out').innerHTML  = ReverseArrayNumber(5)
+document.querySelector('.out-2').innerHTML  = ReverseArrayNumbers(10)
 
 function ReverseArrayNumber (n){
     let arr = []
@@ -10,5 +11,15 @@ function ReverseArrayNumber (n){
     
     return arr
 }
+function ReverseArrayNumbers (n){
+    let arr = []
+
+    for (let i = n; i > 0; i--){
+        arr.push(i)
+    }
+    
+    return arr
+}
 
 console.log(ReverseArrayNumber(5))
+console.log(ReverseArrayNumbers(15))
